318559e02c5915a9597132cf3c2b11df2c48eacb
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-01-25 17:19:29 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🔐 Dragon-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+   f"⚙️ Bot Name : Razer-Userbot\n🔰 Bot ver : V {BOT_VER}\n⏰ Status : Aktif ✅")
 
 
 if len(argv) not in (1, 3, 4):
