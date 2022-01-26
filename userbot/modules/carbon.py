0a03240aff45c94cd4212d830a8a5f10af1ffb97
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-01-26 15:26:07 +0800

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -344,7 +344,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
+        \na Join Channel [KENZHU](https://t.me/triplenineee)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
